28349c8d17f72798b98f63e13a7c11902d1c78a0
muhammadahmedzahid <ahmedzahid212121@gmail.com>
2020-09-04 21:56:39 +0500

    .

--- Document_Object_Model.js
+++ Document_Object_Model.js
@@ -25,3 +25,27 @@ console.log(para);
 
 const parass = document.querySelectorAll('.error');
 console.log(parass);
+
+// Others ways to get the HTML Object in the JavaScript
+// But most of the time we ise querySelector and querySelectorAll
+
+// Get an element by ID
+
+const title = document.getElementById('page-title');
+console.log(title);
+
+// get element by their class name
+
+const err = document.getElementsByClassName('error');
+console.log(err);
+
+// get elements by their tag name
+
+const par = document.getElementsByTagName('p');
+console.log(par);
+
+// One of the drawback of the above methods is that.
+// We cannot use the ForEach Methods with that.
+// We can iterate using the index to access the each element.
+
+// Accesing the HTML and Adding and Changing Page Content.
