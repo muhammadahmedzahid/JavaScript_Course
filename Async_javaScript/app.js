87e83aa19ef5dab1248edeffef402e08374378f3
muhammadahmedzahid <ahmedzahid212121@gmail.com>
2020-09-10 17:38:37 +0500

    After Completion of Asynchoronous javaScript

--- Async_javaScript/app.js
+++ Async_javaScript/app.js
@@ -70,7 +70,7 @@ request.send();
 // Lecture # 05 Callback
 // A little bit more reuseable code with adding the callback functions
 
-const getTodos = (callback) => {
+let getTodos = (callback) => {
   const req = new XMLHttpRequest();
   req.addEventListener('readystatechange', () => {
 
@@ -118,3 +118,221 @@ console.log(4);
 // Everything in the JSON has to be in Double Quotes.
 // a number may or may not be written in the double quotes.
 // Also booleans we cannot use that in strings also.
+
+// Lecture # 07  Callback Hell
+
+// When we have a scenario that when one result comes from a API and then we pass that data into
+// another API and perform task and then so on so how to do that.
+// So we write callback as you see in below but its not looks neat and nice as if we have
+// a lot of callbacks so what's the solution for that the solution is Promises which we study
+// in the next 8th Lecture
+// We put a argument as below resource to add multiple requests.
+getTodos = (resource,callback) => {
+
+  const r = new XMLHttpRequest();
+
+  r.addEventListener('readystatechange', () => {
+
+    if(r.readyState === 4 && r.status === 200){
+      const d = JSON.parse(r.responseText);
+      callback(undefined, d);
+    }else if(r.readyState === 4){
+      callback('could not fetch data', undefined);
+    }
+  });
+
+  r.open('GET', resource);
+  r.send();
+
+};
+
+getTodos('muhammad.json',(err,data) => {
+  console.log('First Function: ',data);
+  getTodos('ahmed.json', (err, data) => {
+    console.log('Second Function: ',data);
+    getTodos('zahid.json', (err, data) => {
+      console.log('Third Function: ',data);
+
+    });
+  });
+
+});
+
+// Lecture # 08 Promises Basics
+
+// When we write the function at that we have to return new Promise() what actually it does.
+// It returns the one thing either it completes its promise means returns the data.
+// or it does not complete the promise means there is some kind of error in it.
+// Promise take a call back function where it takes two argumnets it is not created by ourself.
+// Rather it is created by the Promises API named as reslove and reject.
+// We use if statement to run the resolve and reject depends on the data we get.
+// We use then keyword with the function where we use the Promise keywords.
+// If the resolve gets the data then we fire the first argument.
+// if the reject method called then it calls the second argumnet.
+
+
+
+// Promise Examples
+
+const getSomething = () => {
+  
+  return new Promise((resolve, reject) => {
+    // fetch something from the database
+    // Let say we get the data successfully
+    // resolve('data receieved');
+    // let say data not received
+    reject('error occurs');
+  });
+
+};
+
+// in order to call the function for the promise
+// we use the then keyword
+
+getSomething().then((data) => {
+
+  console.log(data);
+  // if success this argument code runs
+
+}, (err) => {
+  // If there is something error the below code runs.
+  console.log(err);
+});
+
+// the above function calling where we pass two argument function inside the function there is a better way to do that
+// we may use the then and the catch keyword instead
+
+getSomething().then(data => {
+  console.log(data);
+}).catch(err => {
+  console.log(err);
+});
+
+// Now lets do some Practical Example where we read the data and shows the result.
+
+getTodos = (resource) => {
+
+  return new Promise((resolve, reject) => {
+    const re = new XMLHttpRequest();
+
+    re.addEventListener('readystatechange', () => {
+        if(re.readyState === 4 && re.status === 200){
+          const data = JSON.parse(re.responseText);
+          resolve(data)
+        }else if (request.readyState === 4){
+          reject('error getting resource');
+        }
+    });
+    re.open('GET',resource);
+    re.send();
+
+  });
+
+};
+
+// calling the function
+
+getTodos('ahmed.json').then(data => {
+
+  console.log('promise resolved', data);
+}).catch(err => {
+  console.log('promise rejected: ', err);
+});
+
+// Lecture # 09 Chaining Promises
+
+// one of the best thing of the Promises that we can used it with the asynchoronous task.
+// Below is what is chaining in the Promises.
+// one thing we can put only one catch to all the then.
+
+
+getTodos('muhammad.json').then( data => {
+
+  console.log('promise 1 resolved:', data);
+  return getTodos('ahmed.json', data);
+  // after returning the successful of the data we called the other functions.
+}).then(data => {
+  console.log('promise 2 resolved: ',data);
+  return getTodos('zahid.json',data);
+}).then(data => {
+  console.log('promise 3 resolved: ', data);
+}).catch( err => {
+  console.log('promise rejected: ',err);
+});
+
+// Lecture # 10 The Fetch API
+
+// Fetch API: basically when we initially start the HTTP request on that time we used as we done previously XMLHHHTPRequest().
+// But we can make a smarter way of using of Fetch API.
+// It is also handling the Promises by itself that is the plus point of that.
+// For that we use the fetch() and it return a promise which it means it returns the resolve or reject.
+// Important!!! the err in the fetch can only be occur if there is some kind of network error.
+// It cannot err if you are working on the local machine.
+// 404 Resource cannot be found.
+
+fetch('muhammad.json').then((response) => {
+
+  console.log('Promise 1 resolved: ', response);
+  // In the above code after success of the respose the data is saved in the JSON
+  return response.json();
+  // well the above returns the data
+}).then(data => {
+  // the above data we get from the return method and then we go for the another callback
+  console.log(data);
+}).catch((err) => {
+  console.log('rejected',err);
+});
+
+// Lecture # 12 Async & Await
+
+// It changes the promises together
+// When we use the async() function it always returns a promise irrespective of the code in the function.
+// for async() we cannot use the then keyword instead we use the await keyword.
+// what is does it stops the output which we is the respose of the request untill this promise is not resoled.
+// After that we use the response.json() to fetch the data.
+// await keyword has a lot of power as it can chain many things together.
+// and this one is the non-blocking.
+// one time we use the then and catch keyword when we calling hte function as the sync returns a promise so we use the
+// then and catch keyword to resolved that.
+// if we have more data getting than one we use use the same methodology and used the await keyword at the start.
+// Some Browser didnot support the async and await like internet explorer.
+
+getTodos = async () => {
+  const response = await fetch('muhammad.json');
+  const data = await response.json();
+  return data;
+};
+
+console.log(1)
+console.log(2)
+
+getTodos()
+  .then(data => console.log('resolved:', data));
+
+console.log(3)
+console.log(4)
+
+// Lecture # 13 Throwing & Catching Errors
+
+// How we can throw our own errors in the asynchornous functions
+// if there is no network error while using the fetch the rejection cannot occurs.
+// so we can use the throw new Error('to throw error).
+// and we get the data from err.message
+
+getTodos = async () => {
+
+  const response = await fetch('muhammad.json');
+
+  if(response.status !== 200){
+    throw new Error('cannot fetch the data');
+  }
+
+  const data = await response.json();
+
+  return data;
+
+};
+
+getTodos()
+  .then(data => console.log('resolved:', data))
+  .catch(err => console.log('rejected:',err.message));
