83270aac8bd8781efdd08d958f49e9aa1c62fa68
muhammadahmed zahid <ahmedzahid212121@gmail.com>
2020-09-16 09:19:48 +0500

    chat-room starts

---
+++ Real_time_chatroom_Project/scripts/app.js
@@ -0,0 +1,7 @@
+// It is not complarsory that we created the document in the firebase all time.
+
+// Lecture 1 Chat Room Class& Adding Class
+
+// adding new chat documents
+// setting up a real-time listener to get new chats
+// updating the room
