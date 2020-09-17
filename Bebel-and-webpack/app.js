ab243a4f3035c5026f89812fadf0d45e6096712d
muhammadahmed zahid <ahmedzahid212121@gmail.com>
2020-09-17 10:33:30 +0500

    after installing babel and webpacl

---
+++ Bebel-and-webpack/app.js
@@ -0,0 +1,14 @@
+// Every new update is not supported in the new browsers and after developing that feature it take.
+// sometime to update that features so for that we are gonna use the Babel tool this works for us.
+// and in order to organise our code there is a tool called webpack which we going to use with that.
+// We have to install the Babel along with Node.js, NPM node package manager.
+// to check the node installed or not put node -v
+// now our code runs on our computer before it runs into the browser.
+// when we installed node.js it automatically installed the npm manager.
+// packages.json file to track the file into the computer.
+// start with {npm init} type that. then press enter - enter it pops up the result.
+// and now we have package.json file includes in our folder. and it record if any other packages we installed in it.
+// now run that {npm install @babel/core @babel/cli --save-dev}
+// the above command saves it developers code also.
+// it will add this in your json code file.
+// and we can only use that in this project and if we want to use them in order project we have to install them.
